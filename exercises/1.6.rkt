b560a14a25899957450485b3b97b32cfd42745bc
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-07-22 22:55:28 +0000

    add 1.6

---
+++ exercises/1.6.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (square x)
+  (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x)
+  )
+
+; Explanation:
+;
+; Since procedure arguments are evaluated first,
+; sqrt-iter calls itself indefinitely That's why
+; special "if" construct is needed, it evaluates only a particular branch
+; depending on the value of the condition.
